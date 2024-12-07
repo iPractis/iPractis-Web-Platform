00e8f12eb015d6e1d2e85488f0f5c748657a5f62
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2024-12-07 16:14:09 -0600

    Made some touches on navbar teacher and hover effects

--- src/components/Header/Nav/NavDesktopTeacher.jsx
+++ src/components/Header/Nav/NavDesktopTeacher.jsx
@@ -8,8 +8,6 @@ import tutor from "@/public/images/tutor-image-preview.png";
 import notification from "@/public/icons/notification.png";
 import themeLight from "@/public/icons/theme-light.png";
 import logo from "@/public/logos/ipractis-logo-1.png";
-import dashboard from "@/public/icons/dashboard.png";
-import teacher from "@/public/icons/teacher.png";
 import wallet from "@/public/icons/wallet.png";
 import world from "@/public/icons/world.png";
 import add from "@/public/icons/add.png";
@@ -30,23 +28,71 @@ const NavDesktopTeacher = () => {
 
         <div className="flex items-center gap-4">
           <Link
-            className="flex items-center lg:px-4 lg:py-2 gap-3 text-primary-color-P12 ST-SB-4"
+            className="btn btn-quinary flex items-center lg:px-4 rounded-[10px] lg:py-2 gap-3 ST-SB-4 group"
             href="#"
           >
-            <Image
-              alt="Logo iPractis"
-              src={dashboard}
-              className="w-5"
-              priority
-            />{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+            >
+              <rect
+                x="15.7627"
+                y="0.5"
+                width="6.5625"
+                height="6.5625"
+                rx="2"
+                transform="rotate(90 15.7627 0.5)"
+                className="fill-primary-color-P12 group-hover:fill-primary-color-P1"
+              />
+              <rect
+                x="7.3252"
+                y="0.5"
+                width="6.5625"
+                height="6.5625"
+                rx="2"
+                transform="rotate(90 7.3252 0.5)"
+                className="fill-primary-color-P12 group-hover:fill-primary-color-P1"
+              />
+              <rect
+                x="15.7627"
+                y="8.9375"
+                width="6.5625"
+                height="15"
+                rx="2"
+                transform="rotate(90 15.7627 8.9375)"
+                className="fill-primary-color-P12 group-hover:fill-primary-color-P1"
+              />
+            </svg>
             Dashboard
           </Link>
 
           <Link
-            className="flex items-center lg:px-4 lg:py-2 gap-1 text-primary-color-P12 ST-SB-4"
+            className="btn btn-quinary flex items-center lg:px-4 rounded-[10px] lg:py-2 gap-1 ST-SB-4 group"
             href="#"
           >
-            <Image alt="Teacher icon" src={teacher} className="w-5" priority />{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="21"
+              height="20"
+              viewBox="0 0 21 20"
+              fill="none"
+            >
+              <path
+                d="M1.92839 0C1.00791 0 0.261719 0.746192 0.261719 1.66667V11.6667C0.261719 12.5871 1.00791 13.3333 1.92839 13.3333H3.10263C3.85123 11.058 5.76623 9.27286 8.18174 8.61118C6.93447 7.89122 6.09505 6.54364 6.09505 5C6.09505 2.69881 7.96053 0.833333 10.2617 0.833333C12.5629 0.833333 14.4284 2.69881 14.4284 5C14.4284 6.54364 13.589 7.89122 12.3417 8.61118C14.7572 9.27286 16.6722 11.058 17.4208 13.3333H18.5951C19.5155 13.3333 20.2617 12.5871 20.2617 11.6667V1.66667C20.2617 0.746192 19.5155 0 18.5951 0H1.92839Z"
+                className="fill-primary-color-P12 group-hover:fill-primary-color-P1"
+              />
+              <path
+                d="M13.595 5C13.595 6.84095 12.1027 8.33333 10.2617 8.33333C8.42077 8.33333 6.92838 6.84095 6.92838 5C6.92838 3.15905 8.42077 1.66667 10.2617 1.66667C12.1027 1.66667 13.595 3.15905 13.595 5Z"
+                className="fill-primary-color-P12 group-hover:fill-primary-color-P1"
+              />
+              <path
+                d="M16.9284 15.2017C16.9284 18.5347 13.8448 18.3309 10.163 18.3309C6.48105 18.3309 3.59505 18.5347 3.59505 15.2017C3.59505 11.8686 6.57982 9.16667 10.2617 9.16667C13.9436 9.16667 16.9284 11.8686 16.9284 15.2017Z"
+                className="fill-primary-color-P12 group-hover:fill-primary-color-P1"
+              />
+            </svg>
             Find a teacher
           </Link>
         </div>
--- src/components/Header/Nav/NavDestop.jsx
+++ src/components/Header/Nav/NavDestop.jsx
@@ -1,10 +1,9 @@
-import Image from "next/image"
+import Image from "next/image";
 import Link from "next/link";
 
 // Images && icons
 import themeLight from "@/public/icons/theme-light.png";
 import logo from "@/public/logos/ipractis-logo-1.png";
-import whiteDoor from "@/public/icons/white-door.png";
 import world from "@/public/icons/world.png";
 
 const NavDesktop = () => {
@@ -24,10 +23,27 @@ const NavDesktop = () => {
       <div className="flex items-center gap-1.5">
         <Link
           href="/login"
-          className="flex items-center px-4 py-2 gap-1.5 text-primary-color-P12 ST-SB-4"
+          className="btn btn-quinary flex items-center rounded-[10px] px-4 py-2 gap-1.5 ST-SB-4 group"
         >
           Log in{" "}
-          <Image alt="Logo iPractis" src={whiteDoor} className="w-5" priority />
+
+          {/* Svg of log in icon */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="none"
+            height="20"
+            width="20"
+          >
+            <path
+              d="M9.58366 10.5923C9.91316 10.2662 9.91316 9.73389 9.58366 9.40777L7.22663 7.07491C6.70043 6.55411 5.80708 6.92685 5.80708 7.6672V9.16672L2.50033 9.16671C2.04009 9.16671 1.66699 9.53981 1.66699 10C1.66699 10.4603 2.04009 10.8334 2.50033 10.8334L5.80708 10.8334V12.3329C5.80708 13.0733 6.70043 13.446 7.22663 12.9252L9.58366 10.5923Z"
+              className="fill-primary-color-P12 group-hover:fill-primary-color-P1"
+            />
+            <path
+              d="M11.667 3.80138C11.667 3.03661 12.1875 2.36997 12.9294 2.18448L16.2628 1.35112C17.3147 1.08814 18.3337 1.88374 18.3337 2.96803V17.0321C18.3337 18.1164 17.3147 18.912 16.2628 18.649L12.9294 17.8157C12.1875 17.6302 11.667 16.9635 11.667 16.1988V3.80138Z"
+              className="fill-primary-color-P12 group-hover:fill-primary-color-P1"
+            />
+          </svg>
         </Link>
 
         <Link
