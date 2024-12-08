3b8f813199d3ff09cd6363b309a59114f2f6c25b
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2024-12-08 16:25:23 -0600

    Changed export of tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
