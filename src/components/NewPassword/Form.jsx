6b93746dc17ba033efccf66b5deeae9fcf8e560a
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2024-12-13 11:50:52 -0600

    Fixed image is not defined

--- src/components/NewPassword/Form.jsx
+++ src/components/NewPassword/Form.jsx
@@ -4,8 +4,8 @@ import getSecurityLevelMessage from "@/src/lib/utils/getSecurityLevelMessage";
 import CustomNextUiInput from "@/src/components/Globals/CustomNextUiInput";
 import PasswordLevels from "../Register/PasswordLevels";
 import DualButton from "../Globals/DualButton";
-import { AsterisksBGIcon } from "../Icons";
 import { useState } from "react";
+import Image from "next/image";
 
 const Form = () => {
   const [password, setPassword] = useState("");
