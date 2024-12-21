6c8d3928338bbeddfa58b7574c24a26321dce470
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2024-12-21 01:38:55 -0600

    Almost finished the dropdown of navbar desktop teacher

--- src/components/Header/Nav/NavDesktopTeacher.jsx
+++ src/components/Header/Nav/NavDesktopTeacher.jsx
@@ -1,10 +1,16 @@
 import {
   AddIcon,
+  CameraBoxIcon,
+  ChevronRightDoorBoldestIcon,
   ChevronRightDoorBoldIcon,
   DashboardIcon,
+  DialogMarkIcon,
   NewMessageIcon,
   NotificationIcon,
   SearchIcon,
+  SettingsIcon,
+  UserHatIcon,
+  UserIcon,
   UserScreenIcon,
   WalletIcon,
 } from "../../Icons";
@@ -16,10 +22,13 @@ import {
   DropdownTrigger,
   DropdownMenu,
   DropdownItem,
+  RadioGroup,
+  Radio,
 } from "@nextui-org/react";
 
 // Images && icons
 import hamburguer from "@/public/icons/profile-settings-icon-transparent.png";
+import tutorImagePreview from "@/public/images/tutor-image-preview.png";
 import tutor from "@/public/images/tutor-image-preview.png";
 import { logOutUser } from "@/src/lib/actions/authAction";
 import themeLight from "@/public/icons/theme-light.png";
@@ -119,51 +128,211 @@ const NavDesktopTeacher = () => {
 
           <DropdownMenu
             aria-label="Static Actions"
-            variant="faded"
+            variant="light"
             classNames={{
               base: "p-0",
             }}
           >
-            <DropdownItem className="disable-hover" key="logOut">
-              <form action={logOutUser}>
-                <button
-                  type="submit"
-                  className="btn btn-senary group w-full px-4 py-2 rounded-lg flex justify-center items-center"
-                >
-                  <div className="flex-1">
-                    <ChevronRightDoorBoldIcon
-                      fillColor={"fill-primary-color-P12"}
+            <DropdownItem
+              className="bg-primary-color-P2 p-1.5 mb-[30px]"
+              key="topBox"
+              isReadOnly
+            >
+              <div className="flex justify-between items-center">
+                <div className="flex gap-3 items-center">
+                  <div>
+                    <Image
+                      alt={"Tutor Image"}
+                      className="w-[30px] rounded-[10px]"
+                      src={tutorImagePreview}
                     />
                   </div>
 
-                  <span className="sm:flex-[85%] flex-[80%]">Log out</span>
-                </button>
-              </form>
-            </DropdownItem>
-
-            <DropdownItem
-              className="disable-hover mt-[5px] mb-[30px]"
-              key="register"
-            >
-              <Link
-                href="/register"
-                className="btn btn-primary w-full px-4 py-2 rounded-lg"
-              >
-                Register
-              </Link>
-            </DropdownItem>
-
-            <DropdownItem className="disable-hover" key="findTutor">
-              <Link
-                className="btn btn-senary group w-full px-4 py-2 rounded-lg flex justify-center items-center"
-                href="#"
-              >
-                <span className="sm:flex-[85%] flex-[80%]">Find a Tutor</span>
-
-                <div className="flex-1">
-                  <SearchIcon fillColor={"fill-primary-color-P12"} />
+                  <div>
+                    <h2 className="text-primary-color-P12 ST-3">
+                      Hi, Alexandra
+                    </h2>
+                  </div>
                 </div>
-              </Link>
+
+                <RadioGroup
+                  classNames={{
+                    wrapper: "gap-0",
+                  }}
+                  orientation="horizontal"
+                  defaultValue="connected"
+                >
+                  <Radio
+                    className="connected"
+                    classNames={{
+                      wrapper: "w-2.5 h-2.5",
+                      control:
+                        "w-1 h-1 bg-quinary-color-VS5 checked:bg-quinary-color-VS5",
+                    }}
+                    color="connected"
+                    value="connected"
+                  ></Radio>
+
+                  <Radio
+                    className="busy"
+                    classNames={{
+                      wrapper: "w-2.5 h-2.5",
+                      control:
+                        "w-1 h-1 bg-septenary-color-MA5 checked:bg-septenary-color-MA5",
+                    }}
+                    color="busy"
+                    value="busy"
+                  ></Radio>
+
+                  <Radio
+                    className="absent"
+                    classNames={{
+                      wrapper: "w-2.5 h-2.5",
+                      control:
+                        "w-1 h-1 bg-quaternary-color-A5 checked:bg-quaternary-color-A5",
+                    }}
+                    color="absent"
+                    value="absent"
+                  ></Radio>
+                </RadioGroup>
+              </div>
+            </DropdownItem>
+
+            <DropdownItem className="p-0 mb-1.5" key="profile">
+              <button
+                type="button"
+                className="btn btn-quinary rounded-lg flex gap-4 px-4 py-1.5 w-full group"
+              >
+                <UserIcon
+                  fillColor={
+                    "fill-primary-color-P12 group-hover:fill-primary-color-P1"
+                  }
+                />
+
+                <h3>Profile</h3>
+              </button>
+            </DropdownItem>
+
+            <DropdownItem className="p-0 mb-1.5" key="dashboard">
+              <button
+                type="button"
+                className="btn btn-quinary rounded-lg flex gap-4 px-4 py-1.5 w-full group"
+              >
+                <DashboardIcon
+                  fillColor={
+                    "fill-primary-color-P12 group-hover:fill-primary-color-P1"
+                  }
+                />
+
+                <h3>Dashboard</h3>
+              </button>
+            </DropdownItem>
+
+            <DropdownItem className="p-0 mb-1.5" key="classroom">
+              <button
+                type="button"
+                className="btn btn-quinary rounded-lg flex gap-4 px-4 py-1.5 w-full group"
+              >
+                <CameraBoxIcon
+                  fillColor={
+                    "fill-primary-color-P12 group-hover:fill-primary-color-P1"
+                  }
+                  strokeColor={
+                    "stroke-primary-color-P12 group-hover:stroke-primary-color-P1"
+                  }
+                />
+
+                <h3>Classroom</h3>
+              </button>
+            </DropdownItem>
+
+            <DropdownItem className="p-0 mb-1.5" key="findTutor">
+              <button
+                type="button"
+                className="btn btn-quinary rounded-lg flex gap-4 px-4 py-1.5 w-full group"
+              >
+                <SearchIcon
+                  className={
+                    "fill-primary-color-P12 group-hover:fill-primary-color-P1"
+                  }
+                />
+
+                <h3>Find a Tutor</h3>
+              </button>
+            </DropdownItem>
+
+            <DropdownItem className="p-0 mb-1.5" key="favoriteTeachers">
+              <button
+                type="button"
+                className="btn btn-quinary rounded-lg flex gap-4 px-4 py-1.5 w-full group"
+              >
+                <UserHatIcon
+                  fillColor={
+                    "fill-primary-color-P12 group-hover:fill-primary-color-P1"
+                  }
+                />
+
+                <h3>Favorite teachers</h3>
+              </button>
+            </DropdownItem>
+
+            <DropdownItem className="p-0 mb-1.5" key="settings">
+              <button
+                type="button"
+                className="btn btn-quinary rounded-lg flex gap-4 px-4 py-1.5 w-full group"
+              >
+                <SettingsIcon
+                  fillColor={
+                    "fill-primary-color-P12 group-hover:fill-primary-color-P1"
+                  }
+                />
+
+                <h3>Settings</h3>
+              </button>
+            </DropdownItem>
+
+            <DropdownItem className="p-0 my-[30px]" key="becomeTeacher">
+              <button
+                type="button"
+                className="btn btn-quinary rounded-lg flex gap-4 px-4 py-1.5 w-full group"
+              >
+                <UserHatIcon
+                  fillColor={
+                    "fill-primary-color-P12 group-hover:fill-primary-color-P1"
+                  }
+                />
+
+                <h3>Become a teacher</h3>
+              </button>
+            </DropdownItem>
+
+            <DropdownItem className="p-0 mb-1.5" key="assistence">
+              <button
+                type="button"
+                className="btn btn-quinary rounded-lg flex gap-4 px-4 py-1.5 w-full group"
+              >
+                <DialogMarkIcon
+                  fillColor={
+                    "fill-primary-color-P12 group-hover:fill-primary-color-P1"
+                  }
+                />
+
+                <h3>Assistence</h3>
+              </button>
+            </DropdownItem>
+
+            <DropdownItem className="p-0" key="logout">
+              <button
+                type="button"
+                className="btn btn-quinary text-septenary-color-MA6 hover:text-septenary-color-MA6 rounded-lg flex gap-4 px-4 py-1.5 w-full group"
+              >
+                <ChevronRightDoorBoldestIcon
+                  fillColor={"fill-septenary-color-MA6"}
+                  strokeColor={"stroke-septenary-color-MA6"}
+                />
+
+                <h3>Log out</h3>
+              </button>
             </DropdownItem>
           </DropdownMenu>
         </Dropdown>
