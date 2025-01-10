5e1e0177b7562437c2c5485a12eec2e43a1c0f36
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2025-01-09 18:42:43 -0600

    Finished tab status

--- src/components/TeacherRegistration/TabStatus/HowWorksAndSkills.jsx
+++ src/components/TeacherRegistration/TabStatus/HowWorksAndSkills.jsx
@@ -2,57 +2,23 @@ import WhiteSpaceWrapper from "../../Globals/WhiteSpaceWrapper";
 import { AnalyticIcon, ChevronRightIcon } from "../../Icons";
 import SectionHeader from "../../Globals/SectionHeader";
 import DualLink from "../../Globals/DualLink";
+import Link from "next/link";
 
 const HowWorksAndSkills = () => {
   return (
-    <WhiteSpaceWrapper className="space-y-[50px]">
-      <div className="flex flex-col sm:flex-row md:gap-[50px] gap-4">
+    <WhiteSpaceWrapper className="space-y-[50px] sm:p-8 p-0">
+      <div>
         <SectionHeader
-          descriptionText={
-            <>
-              Your application is currently pending approval. We appreciate your
-              patience and will notify you as soon as your application has been
-              reviewed.
-              <span className="block">
-                Feel free to edit your profile to improve it.
-              </span>
-            </>
-          }
-          titleIcon={<AnalyticIcon fillColor={"fill-primary-color-P1"} />}
-          titleText="How it works"
-          titleClassName="MT-SB-1"
-        />
-
-        <SectionHeader
-          descriptionText={
-            <>
-              Your application is currently pending approval. We appreciate your
-              patience and will notify you as soon as your application has been
-              reviewed.
-              <span className="block">
-                Feel free to edit your profile to improve it.
-              </span>
-            </>
-          }
+          descriptionText={"Text"}
           titleIcon={<AnalyticIcon fillColor={"fill-primary-color-P1"} />}
           titleText="Start improving your teaching skills"
           titleClassName="MT-SB-1"
         />
-      </div>
 
-      <DualLink
-        dualLinkWrapper={"flex-col sm:flex-row sm:gap-[50px] gap-4"}
-        leftLinkText={
-          <div className="flex justify-center items-center">
-            <span className="flex-[90%]">Preview my profile</span>
-
-            <div className="flex-1">
-              <ChevronRightIcon fillColor={"fill-primary-color-P12"} />
-            </div>
-          </div>
-        }
-        leftLinkHref={"#"}
-        rightLinkText={
+        <Link
+          className="btn btn-secondary w-full MT-SB-1 rounded-2xl py-3 px-4 mt-[50px]"
+          href={"#"}
+        >
           <div className="flex justify-center items-center">
             <span className="flex-[90%]">Start learning</span>
 
@@ -60,9 +26,30 @@ const HowWorksAndSkills = () => {
               <ChevronRightIcon fillColor={"fill-primary-color-P12"} />
             </div>
           </div>
-        }
-        rightLinkHref={"#"}
-      />
+        </Link>
+      </div>
+
+      <div>
+        <SectionHeader
+          descriptionText={"Text"}
+          titleIcon={<AnalyticIcon fillColor={"fill-primary-color-P1"} />}
+          titleText="How it works"
+          titleClassName="MT-SB-1"
+        />
+
+        <Link
+          className="btn btn-primary w-full MT-SB-1 rounded-2xl py-3 px-4 mt-[50px]"
+          href={"#"}
+        >
+          <div className="flex justify-center items-center">
+            <span className="flex-[90%]">Preview my profile</span>
+
+            <div className="flex-1">
+              <ChevronRightIcon fillColor={"fill-primary-color-P12"} />
+            </div>
+          </div>
+        </Link>
+      </div>
     </WhiteSpaceWrapper>
   );
 };
--- src/components/TeacherRegistration/TabStatus/index.jsx
+++ src/components/TeacherRegistration/TabStatus/index.jsx
@@ -6,9 +6,17 @@ const TabStatus = ({ activeTab }) => {
   return (
     <div className={`${activeTab !== 4 && "hidden"}`}>
       <SectionHeader
-        descriptionText="Your application is currently pending approval. We appreciate your patience and will notify you as soon as your application has been reviewed. Feel free to edit your profile to improve it."
+        descriptionText={
+          <>
+            Your application is currently pending approval. We appreciate your
+            patience and will notify you as soon as your application has been
+            reviewed. <span className="block">Feel free to edit your profile to improve it.</span>
+          </>
+        }
         titleIcon={<AnalyticIcon fillColor={"fill-primary-color-P1"} />}
-        wrapperSectionHeaderClassName={"p-4 mb-[50px]"}
+        wrapperSectionHeaderClassName={
+          "bg-primary-color-P11 rounded-[32px] p-8 mb-[50px]"
+        }
         titleText="Your application is waiting for approval"
         titleClassName="MT-SB-1"
       />
