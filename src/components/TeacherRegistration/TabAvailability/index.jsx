360c6168e40ea2a3e852eff87d4a06fd70a44de4
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2025-01-10 23:38:50 -0600

    Fix cache error on tab availability

--- src/components/TeacherRegistration/TabAvailability/index.jsx
+++ src/components/TeacherRegistration/TabAvailability/index.jsx
@@ -5,15 +5,16 @@ import WorkSchedule from "./WorkSchedule";
 const TabAvailability = ({ activeTab, saved }) => {
   return (
     <div className={`${activeTab !== 3 && "hidden"}`}>
-      {saved ? (
-        <AvailabilityRevision />
-      ) : (
-        <>
-          <WorkTimePreferences />
+      <div className={`${!saved ? "block" : "hidden"}`}>
+        <WorkTimePreferences />
 
-          <WorkSchedule />
-        </>
-      )}
+        <WorkSchedule />
+      </div>
+
+      {/* If user HITS Save button we dissapear top info and make visible bottom one */}
+      <div className={`${saved ? "block" : "hidden"}`}>
+        <AvailabilityRevision />
+      </div>
     </div>
   );
 };
