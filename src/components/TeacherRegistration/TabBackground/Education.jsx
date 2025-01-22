3949954e1e8ec1ec6b653f4a31b3df5d6047c14e
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2025-01-21 21:36:54 -0600

    Add IndividualEducation component for managing educational experience in TeacherRegistration

--- src/components/TeacherRegistration/TabBackground/Education.jsx
+++ src/components/TeacherRegistration/TabBackground/Education.jsx
@@ -1,36 +1,36 @@
-import CustomNextUiTextarea from "../../Globals/CustomNextUiTextarea";
-import InputBGWrapperIcon from "../../Globals/InputBGWrapperIcon";
-import CustomNextUiInput from "../../Globals/CustomNextUiInput";
 import WhiteSpaceWrapper from "../../Globals/WhiteSpaceWrapper";
 import SectionHeader from "../../Globals/SectionHeader";
 
-import { useState } from "react";
-
 // Icons
-import {
-  AddBoxIcon,
-  CalendarAddIcon,
-  CalendarCloseIcon,
-  ChevronDownIcon,
-  GraduationCapIcon,
-  TopArrowCloudIcon,
-  TrashBinIcon,
-  UserHatBiggerIcon,
-} from "../../Icons";
+import { AddBoxIcon, GraduationCapIcon } from "../../Icons";
 
-// Next ui imports
-import {
-  Calendar,
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
-  Button,
-  DropdownItem,
-} from "@nextui-org/react";
+import IndividualEducation from "./IndividualEducation";
 
-const Education = () => {
-  const [dateFromSelected, setDateFromSelected] = useState("");
-  const [dateToSelected, setDateToSelected] = useState("");
+const Education = ({ setEducations, educations }) => {
+  // ADD EDUCATION
+  const handleAddEducation = () => {
+    const newEducation = {
+      company: "",
+      from: "",
+      to: "",
+      description: "",
+    };
+    setEducations([...educations, newEducation]);
+  };
+
+  // UPDATE EDUCATION
+  const handleUpdateEducation = (index, updatedEducation) => {
+    const updatedEducations = educations.map((exp, i) =>
+      i === index ? updatedEducation : exp
+    );
+    setEducations(updatedEducations);
+  };
+
+  // DELETE EDUCATION
+  const handleDeleteEducation = (index) => {
+    const updatedEducations = educations.filter((_, i) => i !== index);
+    setEducations(updatedEducations);
+  };
 
   return (
     <div>
@@ -47,8 +47,9 @@ const Education = () => {
       <WhiteSpaceWrapper className={"bg-primary-color-P12 pb-0 pt-[50px]"}>
         {/* To create a new Professional Experience */}
         <button
-          type="button"
           className="btn btn-septenary flex items-center justify-between w-full rounded-2xl px-4 py-2.5 mb-8"
+          onClick={handleAddEducation}
+          type="button"
         >
           <span className="MT-1 text-primary-color-P4">
             Add professional experience
@@ -56,170 +57,15 @@ const Education = () => {
           <AddBoxIcon fillColor={"fill-primary-color-P4"} />
         </button>
 
-        {/* Input of company, download, and recycle bin */}
-        <div className="flex gap-2.5">
-          <div className="flex-[70%]">
-            <CustomNextUiInput
-              type="text"
-              name="companyProffesionalExperience"
-              placeholder="Example: Google"
-              startContent={
-                <InputBGWrapperIcon>
-                  <UserHatBiggerIcon fillColor={"fill-primary-color-P4"} />
-                </InputBGWrapperIcon>
-              }
-            />
-          </div>
-
-          <div className="flex-1">
-            <button type="button">
-              <InputBGWrapperIcon
-                className={
-                  "btn-septenary rounded-2xl bg-primary-color-P11 w-[48px] h-[48px]"
-                }
-              >
-                <TopArrowCloudIcon fillColor={"fill-primary-color-P4"} />
-              </InputBGWrapperIcon>
-            </button>
-          </div>
-
-          <div className="flex-1">
-            <button type="button">
-              <InputBGWrapperIcon
-                className={
-                  "btn-septenary rounded-2xl bg-primary-color-P11 w-[48px] h-[48px]"
-                }
-              >
-                <TrashBinIcon
-                  strokeColor={"stroke-primary-color-P4"}
-                  fillColor={"fill-primary-color-P4"}
-                />
-              </InputBGWrapperIcon>
-            </button>
-          </div>
-        </div>
-
-        {/* Calendars FROM and TO */}
-        <div className="flex items-center gap-2.5 my-2.5">
-          <div className="flex-1">
-            <div className="flex items-center gap-2">
-              <div className="flex-[50%]">
-                <CustomNextUiInput
-                  isReadOnly
-                  type="text"
-                  value={dateFromSelected}
-                  name="fromCalendar"
-                  placeholder="From"
-                  className="pointer-events-none"
-                  startContent={
-                    <InputBGWrapperIcon>
-                      <CalendarAddIcon fillColor={"fill-primary-color-P4"} />
-                    </InputBGWrapperIcon>
-                  }
-                />
-              </div>
-
-              <div className="flex-1">
-                <Dropdown
-                  classNames={{
-                    content: "p-0",
-                  }}
-                  closeOnSelect={false}
-                >
-                  <DropdownTrigger>
-                    <Button
-                      className="border-0 min-w-fit bg-primary-color-P11 hover:bg-secondary-color-S9 animation-fade flex justify-center items-center w-12 h-12 rounded-2xl"
-                      variant="flat"
-                      type="button"
-                    >
-                      <ChevronDownIcon fillColor={"fill-primary-color-P4"} />
-                    </Button>
-                  </DropdownTrigger>
-
-                  <DropdownMenu
-                    className="p-0 h-0"
-                    itemClasses={{
-                      base: "data-[hover=true]:bg-transparent",
-                    }}
-                  >
-                    <DropdownItem className="p-0">
-                      <Calendar
-                        value={dateFromSelected}
-                        onChange={setDateFromSelected}
-                        disableAnimation
-                      />
-                    </DropdownItem>
-                  </DropdownMenu>
-                </Dropdown>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex-1">
-            <div className="flex items-center gap-2">
-              <div className="flex-[50%]">
-                <CustomNextUiInput
-                  isReadOnly
-                  type="text"
-                  name="fromCalendar"
-                  placeholder="To"
-                  value={dateToSelected}
-                  className="pointer-events-none"
-                  startContent={
-                    <InputBGWrapperIcon>
-                      <CalendarCloseIcon fillColor={"fill-primary-color-P4"} />
-                    </InputBGWrapperIcon>
-                  }
-                />
-              </div>
-
-              <div className="flex-1">
-                <Dropdown
-                  classNames={{
-                    content: "p-0",
-                  }}
-                  closeOnSelect={false}
-                >
-                  <DropdownTrigger>
-                    <Button
-                      className="border-0 min-w-fit bg-primary-color-P11 hover:bg-secondary-color-S9 animation-fade flex justify-center items-center w-12 h-12 rounded-2xl"
-                      variant="flat"
-                      type="button"
-                    >
-                      <ChevronDownIcon fillColor={"fill-primary-color-P4"} />
-                    </Button>
-                  </DropdownTrigger>
-
-                  <DropdownMenu
-                    className="p-0 h-0"
-                    itemClasses={{
-                      base: "data-[hover=true]:bg-transparent",
-                    }}
-                  >
-                    <DropdownItem className="p-0">
-                      <Calendar
-                        value={dateToSelected}
-                        onChange={setDateToSelected}
-                        disableAnimation
-                      />
-                    </DropdownItem>
-                  </DropdownMenu>
-                </Dropdown>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* Enter a text */}
-        <CustomNextUiTextarea
-          classNames={{
-            input: "h-[150px]",
-          }}
-          placeholder="Enter a text"
-          size="primaryiPractis"
-          name="textProfessionalExperience"
-          disableAutosize
-        />
+        {educations?.map((education, index) => (
+          <IndividualEducation
+            key={index}
+            index={index}
+            education={education}
+            handleUpdateEducation={handleUpdateEducation}
+            handleDeleteEducation={handleDeleteEducation}
+          />
+        ))}
       </WhiteSpaceWrapper>
     </div>
   );
---
+++ src/components/TeacherRegistration/TabBackground/IndividualEducation.jsx
@@ -0,0 +1,225 @@
+import CustomNextUiTextarea from "../../Globals/CustomNextUiTextarea";
+import InputBGWrapperIcon from "../../Globals/InputBGWrapperIcon";
+import CustomNextUiInput from "../../Globals/CustomNextUiInput";
+
+// Next ui imports
+import {
+  Calendar,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  Button,
+  DropdownItem,
+} from "@nextui-org/react";
+
+// Icons
+import {
+  CalendarAddIcon,
+  CalendarCloseIcon,
+  ChevronDownIcon,
+  LuggageBiggerIcon,
+  TopArrowCloudIcon,
+  TrashBinIcon,
+} from "../../Icons";
+import { CalendarDate } from "@internationalized/date";
+
+const IndividualEducation = ({
+  index,
+  education,
+  handleUpdateEducation,
+  handleDeleteEducation,
+}) => {
+  const handleInputChange = (field, value) => {
+    const updatedEducation = { ...education, [field]: value };
+    handleUpdateEducation(index, updatedEducation);
+  };
+
+  return (
+    <div className="mb-8">
+      {/* Input of company, download, and recycle bin */}
+      <div className="flex gap-2.5">
+        <div className="flex-[70%]">
+          <CustomNextUiInput
+            value={education?.company}
+            onChange={(e) => handleInputChange("company", e.target.value)}
+            type="text"
+            name="company"
+            placeholder="Example: Google"
+            startContent={
+              <InputBGWrapperIcon>
+                <LuggageBiggerIcon fillColor={"fill-primary-color-P4"} />
+              </InputBGWrapperIcon>
+            }
+          />
+        </div>
+
+        <div className="flex-1">
+          <button type="button">
+            <InputBGWrapperIcon
+              className={
+                "btn-septenary rounded-2xl bg-primary-color-P11 w-[48px] h-[48px]"
+              }
+            >
+              <TopArrowCloudIcon fillColor={"fill-primary-color-P4"} />
+            </InputBGWrapperIcon>
+          </button>
+        </div>
+
+        <div className="flex-1">
+          <button type="button" onClick={() => handleDeleteEducation(index)}>
+            <InputBGWrapperIcon
+              className={
+                "btn-septenary rounded-2xl bg-primary-color-P11 w-[48px] h-[48px]"
+              }
+            >
+              <TrashBinIcon
+                strokeColor={"stroke-primary-color-P4"}
+                fillColor={"fill-primary-color-P4"}
+              />
+            </InputBGWrapperIcon>
+          </button>
+        </div>
+      </div>
+
+      {/* Calendars FROM and TO */}
+      <div className="flex items-center gap-2.5 my-2.5">
+        <div className="flex-1">
+          <div className="flex items-center gap-2">
+            <div className="flex-[50%]">
+              <CustomNextUiInput
+                isReadOnly
+                type="text"
+                value={education?.from}
+                name="fromProfessionalExperience"
+                placeholder="From"
+                className="pointer-events-none"
+                startContent={
+                  <InputBGWrapperIcon>
+                    <CalendarAddIcon fillColor={"fill-primary-color-P4"} />
+                  </InputBGWrapperIcon>
+                }
+              />
+            </div>
+
+            <div className="flex-1">
+              <Dropdown
+                classNames={{
+                  content: "p-0",
+                }}
+                closeOnSelect={false}
+              >
+                <DropdownTrigger>
+                  <Button
+                    className="border-0 min-w-fit bg-primary-color-P11 hover:bg-secondary-color-S9 animation-fade flex justify-center items-center w-12 h-12 rounded-2xl"
+                    variant="flat"
+                    type="button"
+                  >
+                    <ChevronDownIcon fillColor={"fill-primary-color-P4"} />
+                  </Button>
+                </DropdownTrigger>
+
+                <DropdownMenu
+                  className="p-0 h-0"
+                  itemClasses={{
+                    base: "data-[hover=true]:bg-transparent",
+                  }}
+                >
+                  <DropdownItem className="p-0">
+                    <Calendar
+                      onChange={(date) => {
+                        let validDate = new CalendarDate(
+                          date?.year,
+                          date?.month,
+                          date?.day
+                        );
+
+                        handleInputChange("from", validDate?.toString());
+                      }}
+                      disableAnimation
+                    />
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-1">
+          <div className="flex items-center gap-2">
+            <div className="flex-[50%]">
+              <CustomNextUiInput
+                isReadOnly
+                type="text"
+                name="toProfessionalExperience"
+                placeholder="To"
+                value={education?.to}
+                className="pointer-events-none"
+                startContent={
+                  <InputBGWrapperIcon>
+                    <CalendarCloseIcon fillColor={"fill-primary-color-P4"} />
+                  </InputBGWrapperIcon>
+                }
+              />
+            </div>
+
+            <div className="flex-1">
+              <Dropdown
+                classNames={{
+                  content: "p-0",
+                }}
+                closeOnSelect={false}
+              >
+                <DropdownTrigger>
+                  <Button
+                    className="border-0 min-w-fit bg-primary-color-P11 hover:bg-secondary-color-S9 animation-fade flex justify-center items-center w-12 h-12 rounded-2xl"
+                    variant="flat"
+                    type="button"
+                  >
+                    <ChevronDownIcon fillColor={"fill-primary-color-P4"} />
+                  </Button>
+                </DropdownTrigger>
+
+                <DropdownMenu
+                  className="p-0 h-0"
+                  itemClasses={{
+                    base: "data-[hover=true]:bg-transparent",
+                  }}
+                >
+                  <DropdownItem className="p-0">
+                    <Calendar
+                      onChange={(date) => {
+                        let validDate = new CalendarDate(
+                          date?.year,
+                          date?.month,
+                          date?.day
+                        );
+
+                        handleInputChange("to", validDate?.toString());
+                      }}
+                      disableAnimation
+                    />
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Enter a text */}
+      <CustomNextUiTextarea
+        value={education?.description}
+        onChange={(e) => handleInputChange("description", e.target.value)}
+        classNames={{
+          input: "h-[150px]",
+        }}
+        placeholder="Enter a text"
+        size="primaryiPractis"
+        name="descriptionExperience"
+        disableAutosize
+      />
+    </div>
+  );
+};
+
+export default IndividualEducation;
