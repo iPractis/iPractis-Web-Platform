42d411d26476366f53b562ba371177787c136f92
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2025-01-23 09:05:30 -0600

    Add SectionHeader component to TabDashboard for improved layout and clarity

--- src/components/AccountSettings/TabDashboard/index.jsx
+++ src/components/AccountSettings/TabDashboard/index.jsx
@@ -1,5 +1,19 @@
+import SectionHeader from "../../Globals/SectionHeader";
+import { DocumentIcon } from "../../Icons";
+
 const TabDashboard = ({ activeTab }) => {
-    return <div className={`${activeTab !== 0 && "hidden"}`}>TabDashboard</div>;
-  };
-  
-  export default TabDashboard;
+  return (
+    <div className={`${activeTab !== 0 && "hidden"} space-y-8`}>
+      <SectionHeader
+        titleIcon={<DocumentIcon fillColor={"fill-primary-color-P1"} />}
+        wrapperSectionHeaderClassName="bg-quaternary-color-A10 rounded-[32px] p-8"
+        descriptionText={"Do an important thing"}
+        descriptionClassName={"mt-[4px]"}
+        titleClassName="MT-SB-1"
+        titleText={"Important"}
+      />
+    </div>
+  );
+};
+
+export default TabDashboard;
