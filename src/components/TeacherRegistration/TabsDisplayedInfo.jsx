4537a83762db520a7e988b5549f712c853e445a3
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2025-02-04 09:07:59 -0600

    Refactor tabProfileSchema for improved validation in TeacherRegistration; move schema definition to a separate file and enhance error messages for user input

--- src/components/TeacherRegistration/TabsDisplayedInfo.jsx
+++ src/components/TeacherRegistration/TabsDisplayedInfo.jsx
@@ -10,73 +10,14 @@ import { CalendarDate, parseDate } from "@internationalized/date";
 import { useState } from "react";
 import axios from "axios";
 
+// Zod schemas
+import { tabProfileSchema } from "@/src/lib/schema";
+
 // Images && icons
 import ukFlag from "@/public/flags/united-kingdom.png";
 import italyFlag from "@/public/flags/italy.png";
 import franceFlag from "@/public/flags/france.png";
 import spainFlag from "@/public/flags/spain.png";
-
-import { z } from "zod";
-
-const tabProfileSchema = z.object({
-  firstName: z
-    .string()
-    .trim()
-    .min(1, {
-      message: "Invalid field --- Must contain 3 or more characters long.",
-    })
-    .regex(/^[^*^%$£]*$/, {
-      message: "Invalid character --- Your name contains invalid characters.",
-    }),
-  middleName: z
-    .string()
-    .trim()
-    .min(1, {
-      message: "Invalid field --- Must contain 3 or more characters long.",
-    })
-    .regex(/^[^*^%$£]*$/, {
-      message: "Invalid character --- Your name contains invalid characters.",
-    }),
-  lastName: z
-    .string()
-    .trim()
-    .min(1, {
-      message: "Invalid field --- Must contain 3 or more characters long.",
-    })
-    .regex(/^[^*^%$£]*$/, {
-      message: "Invalid character --- Your name contains invalid characters.",
-    }),
-  introduction: z
-    .string()
-    .trim()
-    .min(1, {
-      message: "Invalid field --- Must contain 3 or more characters long.",
-    })
-    .regex(
-      /^(?!.*\b\d{7,15}\b)(?!.*\b[A-Za-z0-9._%+-]+@gmail\.com\b)(?!.*\+\d{1,4}[\s-]?\d{4,}\b)[\s\S]+$/,
-      {
-        message:
-          "Don't share your contact ID --- Your introduction cannot include personal contact details, such as phone numbers, email addresses, or social media IDs, to ensure platform security and privacy compliance.",
-      }
-    ),
-  gender: z.enum(["male", "female"], {
-    message: "Invalid field --- Must choose a gender.",
-  }),
-  languages: z
-    .array(
-      z.object({
-        name: z.string(),
-        level: z
-          .string()
-          .min(1, { message: "Invalid field --- Level is required." }),
-      })
-    )
-    .min(1, { message: "Invalid field --- At least one language is required." })
-    .max(6, {
-      message:
-        "Language Limit Reached --- You can only select up to 6 mastered languages.",
-    }),
-});
 
 const TabsDisplayedInfo = ({
   setActiveTab,
