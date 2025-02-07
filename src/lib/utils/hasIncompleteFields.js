de5a0c310d11dfdb7daee26b227e7c849a70dbc5
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2025-02-06 18:13:49 -0600

    Enhance hasIncompleteFields utility to handle empty arrays in predicate check

--- src/lib/utils/hasIncompleteFields.js
+++ src/lib/utils/hasIncompleteFields.js
@@ -1,7 +1,7 @@
 export const hasIncompleteFields = (
   fieldsFn,
   object,
-  predicate = (field) => field === "" || field === false
+  predicate = (field) => field === "" || field === false || field?.length === 0
 ) => {
   return Object.values(fieldsFn(object)).some(predicate);
 };
