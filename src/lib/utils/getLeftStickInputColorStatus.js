0b5458708cdb67a0f346f053d6247a24a7fa087c
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2025-02-18 13:45:01 -0600

    Making code more readable for juniors

--- src/lib/utils/getLeftStickInputColorStatus.js
+++ src/lib/utils/getLeftStickInputColorStatus.js
@@ -1,8 +1,8 @@
 export const getLeftStickInputColorStatus = (frontEndErrors, backEndErrors, inputValue) => {
   switch (true) {
-    case Boolean(frontEndErrors?.email?.type || backEndErrors?.message):
+    case frontEndErrors?.email?.type || backEndErrors?.message:
       return "bg-septenary-color-MA5";
-    case Boolean(!frontEndErrors?.email?.type && !backEndErrors?.message && inputValue):
+    case !frontEndErrors?.email?.type && !backEndErrors?.message && inputValue:
       return "bg-quinary-color-VS5";
     default:
       return "bg-primary-color-P11 group-hover:bg-quaternary-color-A5";
