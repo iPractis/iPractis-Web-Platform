bc75aca698aa3f95d6e18751ab86377ca351823a
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2025-02-24 11:17:56 -0600

    Implemented orange stick color (default one) in all inputs fields (globally)

--- src/lib/utils/getLeftStickInputColorStatus.js
+++ src/lib/utils/getLeftStickInputColorStatus.js
@@ -1,10 +1,10 @@
-export const getLeftStickInputColorStatus = (frontEndErrors, backEndErrors, inputValue, propertyName) => {
+export const getLeftStickInputColorStatus = (frontEndErrors, backEndErrors, inputValue, propertyName, requiredFieldColor = true) => {
   switch (true) {
     case Boolean(frontEndErrors?.[propertyName]?.type || backEndErrors?.message):
       return "bg-septenary-color-MA5";
     case Boolean(!frontEndErrors?.[propertyName]?.type && !backEndErrors?.message && inputValue):
       return "bg-quinary-color-VS5";
     default:
-      return "bg-primary-color-P11 group-hover:bg-quaternary-color-A5";
+      return `${requiredFieldColor ? "bg-senary-color-W8" : "bg-primary-color-P11"} group-hover:bg-quaternary-color-A5`;
   }
 };
