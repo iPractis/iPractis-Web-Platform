8f097337e3f8be92bbff608e6200fae6af766979
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2025-03-19 11:11:01 -0600

    Add language and timezone selection to account preferences

--- src/components/AccountSettings/TabAccount/Preferences.jsx
+++ src/components/AccountSettings/TabAccount/Preferences.jsx
@@ -1,8 +1,8 @@
 import { getInputStatusBorder } from "@/src/lib/utils/getInputStatusBorder";
 import { SplitDynamicErrorZod } from "@/src/lib/utils/getZodValidations";
+import { languages, timeZones } from "@/src/data/dataAccountSettings";
 import InputLeftStickStatus from "../../Shared/InputLeftStickStatus";
 import InputBGWrapperIcon from "../../Shared/InputBGWrapperIcon";
-import { timeZones } from "@/src/data/dataTeacherRegistration";
 import SectionHeader from "../../Shared/SectionHeader";
 
 // External imports
@@ -13,10 +13,17 @@ import { useController } from "react-hook-form";
 import { useState } from "react";
 
 // Icons
-import { ChevronDownBigIcon, Clock5Icon, SparkleIcon } from "../../Icons";
+import {
+  ChevronDownBigIcon,
+  Clock5Icon,
+  EarthBorderedIcon,
+  QuestionMark,
+  SparkleIcon,
+} from "../../Icons";
 
 const Preferences = ({ errors, control }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpenTimezone, setIsOpenTimezone] = useState(false);
+  const [isOpenLanguage, setIsOpenLanguage] = useState(false);
 
   const {
     field: timeZone,
@@ -26,10 +33,18 @@ const Preferences = ({ errors, control }) => {
     control,
   });
 
+  const {
+    field: language,
+    fieldState: { error: languageError },
+  } = useController({
+    name: "language",
+    control,
+  });
+
   return (
-    <>
+    <div>
       <SectionHeader
-        wrapperSectionHeaderClassName="bg-primary-color-P11 px-4 rounded-[32px] !p-[32px]"
+        wrapperSectionHeaderClassName="bg-primary-color-P11 px-4 rounded-[32px] !p-[32px] mb-16"
         titleIcon={<SparkleIcon fillColor={"fill-primary-color-P1"} />}
         descriptionText={"Customize your account settings."}
         descriptionClassName={"mt-[4px]"}
@@ -37,67 +52,144 @@ const Preferences = ({ errors, control }) => {
         titleClassName="MT-SB-1"
       />
 
-      <InputLeftStickStatus
-        inputBarStatusClassName={getInputStatusBorder(
-          errors,
-          timeZone.value,
-          "timeZone"
-        )}
-      >
-        <Select
-          name="timeZone"
-          placeholder="Select a time zone"
-          selectorIcon={<span></span>}
-          isOpen={isOpen}
-          startContent={
-            <InputBGWrapperIcon>
-              <Clock5Icon fillColor={"fill-primary-color-P4"} />
-            </InputBGWrapperIcon>
-          }
-          endContent={
-            <InputBGWrapperIcon>
-              <ChevronDownBigIcon fillColor={"fill-primary-color-P1"} />
-            </InputBGWrapperIcon>
-          }
-          defaultSelectedKeys={new Set([timeZone.value])}
-          onSelectionChange={(keys) => {
-            const key = Array.from(keys)[0];
-            timeZone.onChange(key);
-          }}
-          onOpenChange={(open) => {
-            setIsOpen(open);
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-[50px] lg:px-8">
+        {/* language */}
+        <div>
+          <InputLeftStickStatus
+            inputBarStatusClassName={getInputStatusBorder(
+              errors,
+              language.value,
+              "language"
+            )}
+          >
+            <Select
+              name="language"
+              placeholder="Select a language"
+              selectorIcon={<span></span>}
+              isOpen={isOpenLanguage}
+              label={
+                <div className="ps-0 mb-2">
+                  <p className="flex gap-1.5 items-center text-primary-color-P4 MT-SB-1">
+                    Language{" "}
+                    <QuestionMark fillColor={"fill-primary-color-P4"} />
+                  </p>
+                </div>
+              }
+              startContent={
+                <InputBGWrapperIcon>
+                  <EarthBorderedIcon strokeColor={"stroke-primary-color-P4"} />
+                </InputBGWrapperIcon>
+              }
+              endContent={
+                <InputBGWrapperIcon>
+                  <ChevronDownBigIcon fillColor={"fill-primary-color-P1"} />
+                </InputBGWrapperIcon>
+              }
+              defaultSelectedKeys={new Set([language.value])}
+              onSelectionChange={(keys) => {
+                const key = Array.from(keys)[0];
+                language.onChange(key);
+              }}
+              onOpenChange={(open) => {
+                setIsOpenLanguage(open);
 
-            if (!open) {
-              timeZone.onBlur();
-            }
-          }}
-          classNames={{
-            trigger: [
-              "select-wrapper-ipractis",
-              timeZoneError?.message && "form-input-error",
-            ],
-            innerWrapper: ["select-ipractis", "w-full"],
-            value: [
-              "group-data-[has-value=true]:text-primary-color-P4 text-primary-color-P4 ST-3",
-            ],
-            listbox: ["text-primary-color-P4"],
-          }}
-        >
-          {timeZones?.map((tz) => (
-            <SelectItem key={tz.value} value={tz.value}>
-              {tz.label}
-            </SelectItem>
-          ))}
-        </Select>
-      </InputLeftStickStatus>
+                if (!open) {
+                  language.onBlur();
+                }
+              }}
+              classNames={{
+                trigger: [
+                  "select-wrapper-ipractis min-h-fit",
+                  languageError?.message && "form-input-error",
+                ],
+                innerWrapper: ["select-ipractis", "w-full", "!pt-0"],
+                value: [
+                  "group-data-[has-value=true]:text-primary-color-P4 text-primary-color-P4 ST-3",
+                ],
+                listbox: ["text-primary-color-P4"],
+                label: "mb-8",
+              }}
+            >
+              {languages?.map((tz, index) => (
+                <SelectItem key={index} value={tz}>
+                  {tz}
+                </SelectItem>
+              ))}
+            </Select>
+          </InputLeftStickStatus>
 
-      <SplitDynamicErrorZod
-        message={
-          timeZoneError?.message &&
-          "Invalid timezone --- Please provide a timezone from select."
-        }
-      />
-    </>
+          <SplitDynamicErrorZod message={languageError?.message} />
+        </div>
+
+        {/* Timezone */}
+        <div>
+          <InputLeftStickStatus
+            inputBarStatusClassName={getInputStatusBorder(
+              errors,
+              timeZone.value,
+              "timeZone"
+            )}
+          >
+            <Select
+              name="timeZone"
+              placeholder="Select a time zone"
+              selectorIcon={<span></span>}
+              isOpen={isOpenTimezone}
+              label={
+                <div className="ps-0 mb-2">
+                  <p className="flex gap-1.5 items-center text-primary-color-P4 MT-SB-1">
+                    Timezone{" "}
+                    <QuestionMark fillColor={"fill-primary-color-P4"} />
+                  </p>
+                </div>
+              }
+              startContent={
+                <InputBGWrapperIcon>
+                  <Clock5Icon fillColor={"fill-primary-color-P4"} />
+                </InputBGWrapperIcon>
+              }
+              endContent={
+                <InputBGWrapperIcon>
+                  <ChevronDownBigIcon fillColor={"fill-primary-color-P1"} />
+                </InputBGWrapperIcon>
+              }
+              defaultSelectedKeys={new Set([timeZone.value])}
+              onSelectionChange={(keys) => {
+                const key = Array.from(keys)[0];
+                timeZone.onChange(key);
+              }}
+              onOpenChange={(open) => {
+                setIsOpenTimezone(open);
+
+                if (!open) {
+                  timeZone.onBlur();
+                }
+              }}
+              classNames={{
+                trigger: [
+                  "select-wrapper-ipractis min-h-fit",
+                  timeZoneError?.message && "form-input-error",
+                ],
+                innerWrapper: ["select-ipractis", "w-full", "!pt-0"],
+                value: [
+                  "group-data-[has-value=true]:text-primary-color-P4 text-primary-color-P4 ST-3",
+                ],
+                listbox: ["text-primary-color-P4"],
+                label: "mb-8",
+              }}
+            >
+              {timeZones?.map((tz) => (
+                <SelectItem key={tz.value} value={tz.value}>
+                  {tz.label}
+                </SelectItem>
+              ))}
+            </Select>
+          </InputLeftStickStatus>
+
+          <SplitDynamicErrorZod message={timeZoneError?.message} />
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/data/dataAccountSettings.js
+++ src/data/dataAccountSettings.js
@@ -32,3 +32,43 @@ export const tabsButtons = [
     Icon: WalletMediumIcon,
   },
 ];
+
+export const languages = [
+  "Spanish",
+  "English",
+  "French",
+  "Arabic",
+  "Japenese",
+  "Italian",
+  "Portuguese",
+];
+
+export const timeZones = [
+  { label: "GMT-12", value: "Etc/GMT+12" },
+  { label: "GMT-11", value: "Pacific/Midway" },
+  { label: "GMT-10", value: "Pacific/Honolulu" },
+  { label: "GMT-9", value: "America/Anchorage" },
+  { label: "GMT-8", value: "America/Los_Angeles" },
+  { label: "GMT-7", value: "America/Denver" },
+  { label: "GMT-6", value: "America/Chicago" },
+  { label: "GMT-5", value: "America/New_York" },
+  { label: "GMT-4", value: "America/Santiago" },
+  { label: "GMT-3", value: "America/Argentina/Buenos_Aires" },
+  { label: "GMT-2", value: "Atlantic/South_Georgia" },
+  { label: "GMT-1", value: "Atlantic/Azores" },
+  { label: "GMT", value: "Etc/UTC" },
+  { label: "GMT+1", value: "Europe/Madrid" },
+  { label: "GMT+2", value: "Europe/Athens" },
+  { label: "GMT+3", value: "Europe/Moscow" },
+  { label: "GMT+4", value: "Asia/Dubai" },
+  { label: "GMT+5", value: "Asia/Karachi" },
+  { label: "GMT+6", value: "Asia/Dhaka" },
+  { label: "GMT+7", value: "Asia/Bangkok" },
+  { label: "GMT+8", value: "Asia/Singapore" },
+  { label: "GMT+9", value: "Asia/Tokyo" },
+  { label: "GMT+10", value: "Australia/Sydney" },
+  { label: "GMT+11", value: "Pacific/Noumea" },
+  { label: "GMT+12", value: "Pacific/Auckland" },
+  { label: "GMT+13", value: "Pacific/Tongatapu" },
+  { label: "GMT+14", value: "Pacific/Kiritimati" },
+];
