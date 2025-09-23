c568c1172d16b288d9a07d0670bbf54df65fba92
Anirudhmadhavkulkarni9094 <anirudhkulkarni9094@gmail.com>
2025-09-24 00:11:46 +0530

    build fix

--- src/components/TeacherRegistration/TabReview/TeacherInfoSpecialties.jsx
+++ src/components/TeacherRegistration/TabReview/TeacherInfoSpecialties.jsx
@@ -11,8 +11,8 @@ const TeacherInfoSpecialties = ({draftData}) => {
         titleText={"Specialties"}
       />
 
-     {draftData.subSubject.map(data=>{
-      return  <WhiteSpaceWrapper className={"bg-primary-color-P11 mt-5"}>
+     {draftData.subSubject.map((data,index)=>{
+      return  <WhiteSpaceWrapper className={"bg-primary-color-P11 mt-5"} key={index}>
         <h3 className="MT-SB-1 text-primary-color-P1 mb-5">{data.selected}</h3>
         <p className="ST-4 text-primary-color-P4">
           {data.description}
