3796f61832252c94b1c96a48e58c82eb01c0246f
Anirudhmadhavkulkarni9094 <anirudhkulkarni9094@gmail.com>
2025-09-25 20:14:28 +0530

    fix

--- src/lib/actions/authAction.js
+++ src/lib/actions/authAction.js
@@ -26,7 +26,7 @@ export async function registerUser(prevState, reactHookFormData) {
 export const logInUser = async (data) => {
   console.log("user login data", data)
   try {
-    const res = await fetch("http://localhost:3000/api/auth/login", {
+    const res = await fetch("https://i-practis-web-platform.vercel.app/api/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
