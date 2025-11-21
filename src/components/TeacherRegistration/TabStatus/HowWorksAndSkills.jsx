e0291638ff91a0e15bebcd9cf68d614ec6209ca0
BhuvanSA <bhuvansa@icloud.com>
2025-11-21 08:31:42 +0530

    fix: Status page to match figma design

--- src/components/TeacherRegistration/TabStatus/HowWorksAndSkills.jsx
+++ src/components/TeacherRegistration/TabStatus/HowWorksAndSkills.jsx
@@ -1,54 +1,67 @@
-import InputBGWrapperIcon from "../../Shared/InputBGWrapperIcon";
 import WhiteSpaceWrapper from "../../Shared/WhiteSpaceWrapper";
-import { AnalyticIcon, ChevronRightIcon } from "../../Icons";
-import SectionHeader from "../../Shared/SectionHeader";
+import { AnalyticIcon, BookOpenedIcon, ChevronDownBigIcon, ChevronRightIcon, WrenchIcon } from "../../Icons";
 import Link from "next/link";
 
 const HowWorksAndSkills = () => {
   return (
     <WhiteSpaceWrapper className="grid lg:grid-cols-2 grid-cols-1 gap-[50px] p-0 lg:px-8">
-      <div>
-        <SectionHeader
-          descriptionText={
-            "Take a quick tour of the platform’s features and see exactly how it supports you in planning lessons, tracking progress, and showcasing achievements."
-          }
-          titleIcon={<AnalyticIcon fillcolor={"fill-primary-color-P1"} />}
-          titleText="How it works"
-          titleClassName="MT-SB-1"
-        />
+      <div className="flex flex-col">
+        <div className="flex items-center gap-1 mb-1">
+          <div className="h-[24px] w-[24px] items-center flex ">
+            <AnalyticIcon fillcolor={"fill-primary-color-P1"} />
+          </div>
+          <h2 className="ST-SB-4 text-primary-color-P1">How it works</h2>
+        </div>
+        <p className="text-primary-color-P4 mb-[50px] ST-3">
+          Take a quick tour of the platform's features and see exactly how it supports you in planning lessons, tracking progress, and showcasing achievements.
+        </p>
 
-        <Link
-          className="btn btn-primary w-full MT-SB-1 rounded-2xl ps-4 p-1.5 mt-[50px] flex justify-between items-center"
-          href={"#"}
-        >
-          <span>I want to know more</span>
+        <div className="mt-[10px]">
+          <Link
+            className="bg-primary-color-P1 w-full MT-SB-1 rounded-[16px] p-[6px] flex items-center justify-between hover:bg-opacity-90 transition-all duration-200"
+            href={"#"}
+          >
+            <div className="flex items-center gap-4">
+              <div className="bg-secondary-color-S11 p-2 rounded-[10px] flex items-center justify-center">
+                <WrenchIcon fillcolor={"fill-primary-color-P1"} />
+              </div>
+              <span className="text-primary-color-P12 ST-SB-4">Learn more</span>
+            </div>
 
-          <InputBGWrapperIcon>
-            <ChevronRightIcon fillcolor={"fill-primary-color-P1"} />
-          </InputBGWrapperIcon>
-        </Link>
+
+            <div className="bg-secondary-color-S11 p-2 rounded-[10px] flex items-center justify-center">
+              <ChevronDownBigIcon fillcolor={"fill-primary-color-P1"} />
+            </div>
+          </Link>
+        </div>
       </div>
 
-      <div className="flex flex-col gap-[50px]">
-        <SectionHeader
-          descriptionText={
-            "Explore interactive lessons, proven strategies, and quick tips to help you make every class session more dynamic."
-          }
-          titleIcon={<AnalyticIcon fillcolor={"fill-primary-color-P1"} />}
-          titleText="Start improving your teaching skills"
-          titleClassName="MT-SB-1"
-        />
+      <div className="flex flex-col">
+        <div className="flex items-center gap-1 mb-1">
+          <div className="h-[24px] w-[24px] items-center flex ">
+            <AnalyticIcon fillcolor={"fill-primary-color-P1"} />
+          </div>
+          <h2 className="ST-SB-4 text-primary-color-P1">Start improving your teaching skills</h2>
+        </div>
+        <p className="text-primary-color-P4 flex-1 ST-3">
+          Explore interactive lessons, proven strategies, and quick tips to help you make every class session more dynamic.
+        </p>
 
         <div className="flex-1 place-content-end">
           <Link
-            className="btn btn-secondary w-full MT-SB-1 rounded-2xl ps-4 p-1.5 flex items-center justify-between"
+            className="bg-tertiary-color-SC6 w-full MT-SB-1 rounded-[16px] p-[6px] flex items-center justify-between hover:bg-opacity-90 transition-all duration-200"
             href={"#"}
           >
-            <span>Start learning</span>
+            <div className="flex items-center gap-4">
+              <div className="bg-secondary-color-S11 p-2 rounded-[10px] flex items-center justify-center">
+                <BookOpenedIcon fillcolor={"fill-tertiary-color-SC6"} />
+              </div>
+              <span className="text-primary-color-P12 ST-SB-4">Start learning</span>
+            </div>
 
-            <InputBGWrapperIcon>
-              <ChevronRightIcon fillcolor={"fill-tertiary-color-SC5"} />
-            </InputBGWrapperIcon>
+            <div className="bg-secondary-color-S11 p-2 rounded-[10px] flex items-center justify-center">
+              <ChevronRightIcon fillcolor={"fill-tertiary-color-SC6"} />
+            </div>
           </Link>
         </div>
       </div>
--- src/components/TeacherRegistration/TabStatus/index.jsx
+++ src/components/TeacherRegistration/TabStatus/index.jsx
@@ -1,31 +1,34 @@
-import SectionHeader from "../../Shared/SectionHeader";
+import { AnalyticIcon,  DocumentIcon } from "../../Icons";
 import HowWorksAndSkills from "./HowWorksAndSkills";
-import { AnalyticIcon, CheckedDocumentIcon } from "../../Icons";
 
 const TabStatus = ({ activeTab }) => {
   return (
     <div className={`${activeTab !== 5 && "hidden"}`}>
       {/* Form completed and sent successfully box */}
-      <SectionHeader
-        descriptionText={"Your form has been successfully submitted! Our team will carefully review your application within 14 days, and we'll update you as soon as possible. Thank you for your patience!"}
-        titleIcon={<CheckedDocumentIcon fillcolor={"fill-primary-color-P1"} />}
-        wrapperSectionHeaderClassName={
-          "bg-[#A3FFBD] rounded-[32px] p-8 mb-[50px]"
-        }
-        titleText="Form completed and sent successfully"
-        titleClassName="MT-SB-1"
-      />
+      <div className="bg-[#A3FFBD] rounded-[32px] p-8 mb-[50px]">
+        <div className="flex items-center gap-1 mb-1">
+          <div className="h-[24px] w-[24px] items-center flex">
+            <DocumentIcon fillcolor={"fill-primary-color-P1"} />
+          </div>
+          <h2 className="ST-SB-4 text-primary-color-P1">Form completed and sent successfully</h2>
+        </div>
+        <p className="text-primary-color-P4 ST-3">
+          Your form has been successfully submitted! Our team will carefully review your application within 14 days, and we'll update you as soon as possible. Thank you for your patience!
+        </p>
+      </div>
 
       {/* Your application is waiting for approval box */}
-      <SectionHeader
-        descriptionText={"Your application is currently pending approval. We appreciate your patience and will notify you as soon as your application has been reviewed. Feel free to edit your profile to improve it."}
-        titleIcon={<AnalyticIcon fillcolor={"fill-primary-color-P1"} />}
-        wrapperSectionHeaderClassName={
-          "bg-primary-color-P11 rounded-[32px] p-8 mb-[50px]"
-        }
-        titleText="Your application is waiting for approval"
-        titleClassName="MT-SB-1"
-      />
+      <div className="bg-primary-color-P11 rounded-[32px] p-8 mb-[50px]">
+        <div className="flex items-center gap-1 mb-1">
+          <div className="h-[24px] w-[24px] items-center flex">
+            <AnalyticIcon fillcolor={"fill-primary-color-P1"} />
+          </div>
+          <h2 className="ST-SB-4 text-primary-color-P1">Your application is waiting for approval</h2>
+        </div>
+        <p className="text-primary-color-P4 ST-3">
+          Your application is currently pending approval. We appreciate your patience and will notify you as soon as your application has been reviewed. Feel free to edit your profile to improve it.
+        </p>
+      </div>
 
       <HowWorksAndSkills />
     </div>
